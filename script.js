664bdd12ff04e5edb754936c84dc661b9ebfbaf9
Kylièn <kbergh@live.nl>
2019-01-18 13:46:54 +0100

    bier

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ $('a[href^="#"]').on('click', function(event) {
         }, 1000);
     }
 });
+
+
+var printonly = $( '.print_only .row' );
+var rows = $( '.row' ).not( printonly );
+
+function scaleImageColumn( ) {
+  rows.each( function( ) {
+      var row = $( this );
+      var lft = row.find( '.leftcolumn' );
+      var rgt = row.find( '.rightcolumn' );
+
+      var imgs = lft.find( 'img.web_only' );
+      imgs.css( 'width', '' );
+
+
+      console.log( lft.height( ) + ' / ' + rgt.height( ) );
+      console.log( imgs );
+
+
+      imgs.css( 'width', ( 100 * ( rgt.height( ) + 20 ) / lft.height( ) ) + '%' );
+  } );
+}
+
+$( window ).resize( scaleImageColumn )
+           .trigger( 'resize' );
